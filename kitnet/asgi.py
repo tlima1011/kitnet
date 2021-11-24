acecef5b2a690b5d399d46fd9954ea8961250663
Thiago <thiagolima1011@gmail.com>
2021-11-24 17:36:41 -0300

    Criacao de projeto de imoveis de site chamado kitnet

---
+++ kitnet/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for kitnet project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kitnet.settings')
+
+application = get_asgi_application()
